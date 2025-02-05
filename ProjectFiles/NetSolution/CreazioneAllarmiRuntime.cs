557d32fcb913d1716840c7b55bc8b805771977fe
Alessio <a.forafo@asem.it>
2025-02-05 17:36:57 +0100

    Creazione allarmi a runtime

---
+++ ProjectFiles/NetSolution/CreazioneAllarmiRuntime.cs
@@ -0,0 +1,35 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.CODESYS;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.Alarm;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+#endregion
+
+public class CreazioneAllarmiRuntime : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+        for (int i = 0; i < 3000; i++)
+        {
+            var allarme = InformationModel.Make<AllarmeDigitaleBW>("Allarme_" + i);
+            allarme.Message = "Testo allarme " + i;
+            allarme.SetAlias("VarPLC", Project.Current.Get("CommDrivers/CODESYSDriver1/CODESYSStation1/Tags/Application/PLC_PRG/MainQueue/" + i));
+            Project.Current.Get("Alarms/AllarmeIstanze").Add(allarme);
+        }
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
